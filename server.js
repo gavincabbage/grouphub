67141bc3a209f6f38c8453237b0193039bcaf353
Gavin Cabbage <gavincabbage@gmail.com>
2015-05-02 04:26:30 +0000

    trying to hit groupme api

--- server.js
+++ server.js
@@ -1,12 +1,38 @@
+// server.js
+// usage: node server.js AUTH_TOKEN
+
 var http = require('http');
-var groupme = require('groupme');
+var groupme = require('groupme').Stateless;
+
+if (process.argv.length !== 3) {
+  process.exit(1);
+}
+
+var token = process.argv[2];
+var group = 13781399;
+var opts = 
+  { 
+    message: 
+    {
+      source_guid: '123', 
+      text: 'test message', 
+      attachments: []
+    }
+  };
+
+var justPrintEverythingCallback = function(err, ret) {
+  if (!err) {
+    console.log(JSON.stringify(ret, null, " "));
+  } else {
+    console.log("ERROR!", err)
+  }
+}
 
 var server = http.createServer(function(req, res) {
-  
   if (req.method === 'POST') {
     console.log('got a POST request');
+    groupme.Messages.create(token, group, opts, justPrintEverythingCallback);
   }
-  
   res.writeHead(200);
   res.end();
 });
