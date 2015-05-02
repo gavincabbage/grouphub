2e836ef21b7c911f6f02634f1ac07210d27aa645
Gavin Cabbage <gavincabbage@gmail.com>
2015-05-02 18:02:14 +0000

    config file

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
-// server.js
-// usage: node server.js AUTH_TOKEN
-
 var fs = require('fs');
 var secrets = JSON.parse(fs.readFileSync('secrets.json', 'utf8'));
+var config = JSON.parse(fs.readFileSync('config.json', 'utf8'));
+var github_config = { path: config.paths.webhook, secret: secrets.secret };
 
 var http = require('http');
 var groupme = require('groupme').Stateless;
-var github = require('github-webhook-handler')({ path: '/', secret: secrets.secret });
-
-var token = process.argv[2];
-var bot_id = 'c79491442177436efbbc76f304';
+var github = require('github-webhook-handler')(github_config);
 
 var botCallback = function(err, ret) {
     if (!err) {
@@ -20,13 +16,12 @@ var botCallback = function(err, ret) {
 }
 
 github.on('push', function (event) {
-    
     console.log('Received a push event for %s to %s from %s',
         event.payload.repository.name,
         event.payload.ref,
         event.payload.pusher.name);
         
-    groupme.Bots.post(token, bot_id, event.payload.pusher.name, {}, botCallback);
+    groupme.Bots.post(secrets.token, secrets.bot_id, event.payload.pusher.name, {}, botCallback);
 })
 
 var server = http.createServer(function (req, res) {
