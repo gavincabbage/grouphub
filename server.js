c92e04b77fd63c19d092f96d56227f392bf3711b
Gavin Cabbage <gavincabbage@gmail.com>
2015-05-02 05:01:30 +0000

    testing bot

--- server.js
+++ server.js
@@ -22,7 +22,6 @@ var justPrintEverythingCallback = function(err, ret) {
 var server = http.createServer(function(req, res) {
   if (req.method === 'POST') {
     console.log('got a POST request');
-    console.log(req);
     var text = req.body;
     console.log('TEXT: \n' + text);
     groupme.Bots.post(token, bot_id, text, {}, justPrintEverythingCallback);
